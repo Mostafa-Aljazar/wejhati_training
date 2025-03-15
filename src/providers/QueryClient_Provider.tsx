52dd91ce227a0770074b448a7f07fbbcd60cb243
Mostafa-Aljazar <mostafaibrahim20032020@gmail.com>
2025-03-15 12:41:55 +0200

    install react-query for asynchronous state management and refetching

---
+++ src/providers/QueryClient_Provider.tsx
@@ -0,0 +1,18 @@
+import {
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+export default function QueryClient_Provider({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    );
+}
--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -3,6 +3,7 @@ import NextIntl_Provider from "./NextIntl_Provider";
 import { AbstractIntlMessages } from "next-intl";
 import Mantine_Provider from "./Mantine_Provider";
 import Nuqs_Adapter from "./Nuqs_Adapter";
+import QueryClient_Provider from "./QueryClient_Provider";
 
 interface Props {
     children: ReactNode
@@ -16,7 +17,9 @@ export default function Provider({ children, locale, messages }: Props) {
         <NextIntl_Provider messages={messages} locale={locale}>
             <Mantine_Provider>
                 <Nuqs_Adapter>
-                    {children}
+                    <QueryClient_Provider>
+                        {children}
+                    </QueryClient_Provider>
                 </Nuqs_Adapter>
             </Mantine_Provider>
         </NextIntl_Provider>
